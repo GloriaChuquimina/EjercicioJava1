5dffa7bf9db4f2db8fb6e0f756b5e42d4a874508
schuquimia <schuquimia@SNPPC-01-201.senape.gob.bo>
2024-11-14 14:28:07 -0400

    Se agregó funcionalidad

--- src/main/java/com/ejercicio1/ejercicio1/Ejercicio1Application.java
+++ src/main/java/com/ejercicio1/ejercicio1/Ejercicio1Application.java
@@ -8,7 +8,8 @@ public class Ejercicio1Application {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(Ejercicio1Application.class, args);
-		System.out.printf("Hello and welcome!");
+		// System.out.printf("Hello and welcome!");
+		System.out.printf("Welcome to JavaBank ATM! Version control with Git.");
 		for (int i=1;i<=5;i++)
 		{
 			System.out.println("i= "+i);
