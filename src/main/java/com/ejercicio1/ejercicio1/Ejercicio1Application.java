6105d87f752aedd9cdd2f8e5610ca713f6331ea6
schuquimia <schuquimia@SNPPC-01-201.senape.gob.bo>
2024-11-14 14:25:16 -0400

    Repositorio de Ejercicio_1 en JAVA

---
+++ src/main/java/com/ejercicio1/ejercicio1/Ejercicio1Application.java
@@ -0,0 +1,19 @@
+package com.ejercicio1.ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1Application {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(Ejercicio1Application.class, args);
+		System.out.printf("Hello and welcome!");
+		for (int i=1;i<=5;i++)
+		{
+			System.out.println("i= "+i);
+
+		}
+	}
+
+}
